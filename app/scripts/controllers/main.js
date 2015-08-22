9c66b258835615a7ed725a8b0f7c75f7e72e1c86
vaibhav <vaibhav.khl@gmail.com>
2015-08-23 00:40:37 +0530

    adding msg details

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,131 +8,143 @@
  * Controller of the gmailAppApp
  */
 angular.module('gmailAppApp')
-  .controller('MainCtrl', function ($scope, $q) {
-    // Your Client ID can be retrieved from your project in the Google
-    // Developer Console, https://console.developers.google.com
-    var CLIENT_ID = '1042316795013-09puaanar8ickpkeq1cnlnp7m3il9p16.apps.googleusercontent.com';
+    .controller('MainCtrl', function ($scope, $q) {
+        // Your Client ID can be retrieved from your project in the Google
+        // Developer Console, https://console.developers.google.com
+        var CLIENT_ID = '1042316795013-09puaanar8ickpkeq1cnlnp7m3il9p16.apps.googleusercontent.com';
 
-    var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+        var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-    $scope.isAuthorized = true;
-    $scope.messages = [];
-    /**
-     * Check if current user has authorized this application.
-     */
-    function checkAuth() {
-      gapi.auth.authorize(
-        {
-          'client_id': CLIENT_ID,
-          'scope': SCOPES,
-          'immediate': true
-        }, handleAuthResult);
-    }
+        $scope.isAuthorized = true;
+        $scope.messages = [];
+        /**
+         * Check if current user has authorized this application.
+         */
+        function checkAuth() {
+            gapi.auth.authorize(
+                {
+                    'client_id': CLIENT_ID,
+                    'scope': SCOPES,
+                    'immediate': true
+                }, handleAuthResult);
+        }
 
-    $scope.authorize = function() {
-    	gapi.auth.authorize(
-        {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-        handleAuthResult);
-      return false;
-    }
+        $scope.authorize = function () {
+            gapi.auth.authorize(
+                {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+                handleAuthResult);
+            return false;
+        };
 
-    checkAuth();
+        checkAuth();
 
-    /**
-       * Handle response from authorization server.
-       *
-       * @param {Object} authResult Authorization result.
-       */
-    function handleAuthResult(authResult) {
-      if (authResult && !authResult.error) {
-      	console.log('authorized')
-      	$scope.isAuthorized = true;
-        loadGmailApi();
-      } else {
-        console.log('not authorized')
-        $scope.isAuthorized = false
-        $scope.$apply();
-      }
-    }
+        /**
+         * Handle response from authorization server.
+         *
+         * @param {Object} authResult Authorization result.
+         */
+        function handleAuthResult(authResult) {
+            if (authResult && !authResult.error) {
+                console.log('authorized');
+                $scope.isAuthorized = true;
+                loadGmailApi();
+            } else {
+                console.log('not authorized');
+                $scope.isAuthorized = false;
+                $scope.$apply();
+            }
+        }
 
-    /**
-     * Load Gmail API client library. List labels once client library
-     * is loaded.
-     */
-    function loadGmailApi() {
-      gapi.client.load('gmail', 'v1', loadData);
-    }
+        /**
+         * Load Gmail API client library. List labels once client library
+         * is loaded.
+         */
+        function loadGmailApi() {
+            gapi.client.load('gmail', 'v1', loadData);
+        }
 
-    function loadData() {
-    	console.log('loading data')
-    	listLabels();
-    	//listMessages();
-    }
+        function loadData() {
+            console.log('loading data');
+            listLabels();
+            //listMessages();
+        }
 
-    function listLabels() {
-      var request = gapi.client.gmail.users.labels.list({
-        'userId': 'me'
-      });
-      request.execute(function(resp) {
-      	$scope.labels = resp.labels;
-      	$scope.$apply()
-      })
-    }
+        function listLabels() {
+            var request = gapi.client.gmail.users.labels.list({
+                'userId': 'me'
+            });
+            request.execute(function (resp) {
+                $scope.labels = resp.labels;
+                $scope.$apply()
+            })
+        }
 
-    $scope.getMsgsForLabel = function(labelId) {
-    	var request = gapi.client.gmail.users.messages.list({
-        'userId': 'me',
-        'labelIds': labelId
-      });
+        $scope.getMsgsForLabel = function (labelId) {
+            var request = gapi.client.gmail.users.messages.list({
+                'userId': 'me',
+                'labelIds': labelId
+            });
 
-      request.execute(function(resp) {
-      	$scope.msgCount = resp.resultSizeEstimate;
-      	getMessages(resp.messages)
-      })
-    }
+            request.execute(function (resp) {
+                $scope.msgCount = resp.resultSizeEstimate;
+                getMessages(resp.messages)
+            })
+        };
 
-    // function listMessages() {
-    //   var request = gapi.client.gmail.users.messages.list({
-    //     'userId': 'me'
-    //   });
+        // function listMessages() {
+        //   var request = gapi.client.gmail.users.messages.list({
+        //     'userId': 'me'
+        //   });
 
-    //   request.execute(function(resp) {
-    //     getMessages(resp.messages);
-    //   })
-    // }
+        //   request.execute(function(resp) {
+        //     getMessages(resp.messages);
+        //   })
+        // }
 
-    function getMessages(msgs) {
-    	// var count = 0;
-    	// var maxCount = msgs.length;
-    	// console.log('max count', maxCount)
+        function getMessages(msgs) {
 
-    	$scope.messages.length = 0;
+            $scope.messages.length = 0;
 
-    	_.each(msgs, function(msg) {
-    		//count = count + 1
+            _.each(msgs, function (msg) {
+                //count = count + 1
 
-    		gapi.client.gmail.users.messages.get({
-			    'userId': 'me',
-			    'id': msg.id
-			  }).then(function(resp) {
-			  	$scope.messages.push(resp);
-			  	$scope.$apply();
-			  	// if (count === maxCount) {
-			  	// 	console.log('got msgs')
-			  	// 	$scope.$apply()
-			  	// };
-			  })
+                gapi.client.gmail.users.messages.get({
+                    'userId': 'me',
+                    'id': msg.id
+                }).then(function (resp) {
+                    buildMsg(resp);
+                })
 
-    	})
-    }
+            })
+        }
 
+        var extractField = function (resp_result, fieldName) {
+            var field = _.find(resp_result.payload.headers, function (header) {
+                return header.name.toLowerCase() === fieldName.toLowerCase();
+            })
+            return field.value
+        };
 
+        function buildMsg(resp) {
+            var m = {};
+            m.from = extractField(resp.result, 'FROM')
+            m.subject = extractField(resp.result, 'SUBJECT')
+            m.date = extractField(resp.result, 'DATE')
+            //m.text = getText(resp.result.payload)
 
-    // function getMsgBody(msg) {
-	  //   var part = msg.parts.filter(function(part) {
-			//   return part.mimeType == 'text/html';
-			// });
-			// var html = urlSafeBase64Decode(part.body.data);
-    // }
+            $scope.messages.push(m);
+            $scope.$apply();
+        }
 
-  });
+        function getText(payload) {
+            console.log(payload)
+            var part = payload.parts.filter(function (part) {
+                return part.mimeType == 'text/html';
+            });
+            console.log('part', part)
+            var html = atob(part.body.data.replace(/-/g, '+').replace(/_/g, '/'));
+            console.log('msg html', html)
+            return html
+        }
+
+    });
